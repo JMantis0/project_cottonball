934359420b251515ba39acb4535c389593a64a33
JMan <60026263+JMantis0@users.noreply.github.com>
2021-03-16 13:57:04 -0700

    Updated express npm package and added test route to API routes.

---
+++ client/routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.get("/test", (req, res) => {
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log("GET request from client: /api/test");
+  console.log("Sending OK status to client.");
+  res.send(200);
+});
+
+export default router;
