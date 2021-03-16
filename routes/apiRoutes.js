5ffd54487bd5cffb9c02a9549c6b21a88e4b8989
JMan <60026263+JMantis0@users.noreply.github.com>
2021-03-16 15:58:55 -0700

    created config directory in root directory.  Created config.json file in config directory to establish connection between sequelize and mysql (via index.js in models directory).  Created a /testCreate route in apiRoutes to test creating entries in the DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
+const models = require("../models");
+
+router.put("/testCreate", (req, res) => {
+  const newTestData = models.TestData.build({ text: "TEST DATA" });
+  newTestData
+    .save()
+    .then(response => {
+      console.log("Saved a new entry to database");
+      res.sendStatus(201);
+    })
+    .catch(error => {
+      console.log(
+        "There was an error attempting to create a test DB entry: ",
+        error
+      );
+      res.status(400).send(error);
+    });
+});
 
 router.get("/test", (req, res) => {
   console.log(
