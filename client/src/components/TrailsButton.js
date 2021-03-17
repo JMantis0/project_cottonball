996547725c4d57ad44a6e84fe1fd32e46e85f61e
JMan <60026263+JMantis0@users.noreply.github.com>
2021-03-17 01:24:55 -0700

    Added npm package dotenv to package.json of root directory.  Replaced config/config.json with /config/config.js.  Config.js exports a commonJS module, which is superior to using a plain json file because plain json files cannot use dotenv to hide passwords.  Modified DarkSkyButton.js and TrailButton.js in the following ways: Moved their respective API calls to the back-end (to routes/apiRoutes.js) and replaced them with api calls to the back end.  This has the effect of allowing the back end to call the DarkSky and TrailsAPI which is superior to calling them from the client because calling them from the back-end allows the api keys to be hidden with dotenv.  Also made a small change to models/index.js to correctly import the config.js that replaces config.json as of this commit

--- client/src/components/TrailsButton.js
+++ client/src/components/TrailsButton.js
@@ -4,33 +4,18 @@ const axios = require("axios").default;
 //  This function sets up an options object and then makes a client-side axios get request to
 //  the Trails API.
 const callTrailApiSeattle = () => {
-  const trailOptions = {
-    method: "GET",
-    url: "https://trailapi-trailapi.p.rapidapi.com/",
-    params: {
-      "q-activities_activity_type_name_eq": "hiking",
-      "q-city_cont": "Seattle",
-      "q-state_cont": "Washington",
-      // The longitude and latitude in the url path are for Seattle, Washington.
-      lat: "47.6062",
-      lon: "-122.3321",
-      radius: "25",
-      //   'q-country_cont': 'United States',
-      limit: "25"
-    },
-    headers: {
-      "x-rapidapi-key": "8ff0e46ef5msh49ddf32c8fc21f9p11dbd5jsn56ad2be3313b",
-      "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-    }
-  };
-
+  console.log("Call Trails API Button Clicked...");
+  console.log("Sending get request to backend");
+  console.log("Expecting backend to call Trails API and return response object");
   axios
-    .request(trailOptions)
-    .then((response) => {
+    .get("/api/callTrailsAPI")
+    .then(response => {
+      console.log("Response from server: ");
       console.log(response.data);
     })
-    .catch((error) => {
-      console.error(error);
+    .catch(error => {
+      console.log("There was an error: ");
+      console.log(error);
     });
 };
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,88 @@
 const router = require("express").Router();
 const models = require("../models");
+const axios = require("axios").default;
+require("dotenv").config();
 
+router.get("/callDarkSky", (req, res) => {
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log("GET request from client: /api/callDarkSky");
+  const options = {
+    method: "GET",
+    // The longitude and latitude in the url path are for Seattle, Washington.
+    url: "https://dark-sky.p.rapidapi.com/47.6062,-122.3321",
+    params: { lang: "en", units: "auto" },
+    headers: {
+      "x-rapidapi-key": process.env.DARKSKY_API_KEY,
+      "x-rapidapi-host": "dark-sky.p.rapidapi.com"
+    }
+  };
+  axios
+    .request(options)
+    .then(response => {
+      console.log("Response from DarkSky: ");
+      console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      console.log("There was an error: ");
+      console.error(error);
+      res.status(500).send(error);
+    });
+});
+
+router.get("/callTrailsAPI", (req, res) => {
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log("GET request from client: /api/callTrailsAPI");
+  const trailOptions = {
+    method: "GET",
+    url: "https://trailapi-trailapi.p.rapidapi.com/",
+    params: {
+      "q-activities_activity_type_name_eq": "hiking",
+      "q-city_cont": "Seattle",
+      "q-state_cont": "Washington",
+      // The longitude and latitude in the url path are for Seattle, Washington.
+      lat: "47.6062",
+      lon: "-122.3321",
+      radius: "25",
+      //   'q-country_cont': 'United States',
+      limit: "25"
+    },
+    headers: {
+      "x-rapidapi-key": process.env.TRAILS_API_KEY,
+      "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+    }
+  };
+  axios
+    .request(trailOptions)
+    .then(response => {
+      console.log("Response from Trails API: ");
+      console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch(error => {
+      console.log("There was an error: ");
+      console.error(error);
+      res.status(500).send(error);
+    });
+});
 router.put("/testCreate", (req, res) => {
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log(
+    "███████████████████████████████████████████████████████████████████"
+  );
+  console.log("PUT request from client: /api/testCreate");
   const newTestData = models.TestData.build({ text: "TEST DATA" });
   newTestData
     .save()
