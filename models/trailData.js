175f458d835f2b0237b6957bfe0cc3b885f07e1a
Kathleen Kelly <murphy.kaite@gmail.com>
2021-03-20 01:31:05 -0700

    trailData model created

---
+++ models/trailData.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes) {
+    let Trail = sequelize.define("Trail", {
+        
+        trail_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        
+        name: {
+            type: DataTypes.STRING
+        },
+        city: {
+            type: DataTypes.STRING   
+        },
+        state: {
+            type: DataTypes.STRING
+        },
+        unique_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        activity_type_name: {
+            type: DataTypes.STRING   
+        },
+        description: {
+            type: DataTypes.STRING   
+        },
+        rating: {
+            type: DataTypes.INTEGER
+        }, 
+        thumbnail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+
+    
+    });
+
+    return Trail;
+}
+
+// places[0].
