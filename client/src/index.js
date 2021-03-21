15b688b3cd5fcba80d0d808943bd61cf41a9e220
JMan <60026263+JMantis0@users.noreply.github.com>
2021-03-21 05:53:29 -0700

    Testing Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -9,13 +9,7 @@ ReactDOM.render(
   <Auth0Provider
     domain="jmantis-auth.us.auth0.com"
     clientId="aNLTSSORpHeSWRsFxjwoTikI5BriwnCn"
-    redirectUri={
-      window.location.href.includes("localhost")
-        ? "http://localhost:3000"
-        : window.location.href.includes("test")
-          ? "https://test-cottonball-auth0.herokuapp.com/"
-          : "https://project-cottonball.herokuapp.com/"
-    }
+    redirectUri="https://test-cottonball-auth0.herokuapp.com/"
   >
     <App />
   </Auth0Provider>,
